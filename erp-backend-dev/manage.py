1be562561330fbb4912fce081e311d0c74af5223
Lakshan <lakshan.m@ritzclothing.lk>
2026-07-09 14:47:23 +0530

    configure production django settings

--- erp-backend-dev/manage.py
+++ erp-backend-dev/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ritzerp.settings.settings')        # ritzerp.settings.settings
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'git ritzerp.settings.lakshan_dev')        # ritzerp.settings.settings
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
